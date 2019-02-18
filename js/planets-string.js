f309c35b74afdc2cbb13d698688b153558af1cff
laroblesaguilar <laroblesaguilar@gmail.com>
2019-02-18 00:35:01 -0600

    completed exercise

--- js/planets-string.js
+++ js/planets-string.js
@@ -29,5 +29,32 @@
     var planetsArray1 = planetsArray.join('<br>');
     console.log(planetsArray1);
 
+    // bonus
+
+    function arrayAdd(array) {
+    var sum = array.reduce(function(accumulator, currentValue) {
+            return (accumulator + currentValue);
+        });
+        return sum;
+    }
+
+    console.log(arrayAdd([1,2,3,4,5,8]));
+
+
+var emptyList = new Array();
+var add = confirm("Did you want to add a student?");
+if (add){
+    var studentName = prompt("Enter student name");
+    emptyList.unshift(studentName);
+    alert("Student list: \n" + emptyList);
+}
+var deleteStudent= confirm("Did you want to delete a student?");
+if(deleteStudent) {
+    var studentDel = prompt("Enter students name to delete: ");
+    delete emptyList[studentDel];
+    alert("Student list: \n" + emptyList);
+
+}
+
 
 })();
