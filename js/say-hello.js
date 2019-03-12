522c357f8755650b2e87867e061faed41e0e57b8
laroblesaguilar <laroblesaguilar@gmail.com>
2019-03-12 09:40:07 -0500

    completed exercise

---
+++ js/say-hello.js
@@ -0,0 +1,3 @@
+export const sayHello = (name = "world") => `Hello, ${name}`;
+
+// module.exports = sayHello;
--- js/simongame.js
+++ js/simongame.js
@@ -2,118 +2,161 @@ $(document).ready(function () {
 var randomNum = Math.floor(Math.random()* 4 +1);
 var colorClick = [];
 var simonGame = [];
-
+// Starts the game
+    setTimeout(function(){
+        randomColor(randomNum)
+    }, 1000)
+    userClick();
 function randomColor(num){
     simonGame.push(num);
     for(var i = 0; i < simonGame.length; i++) {
         if (simonGame[i] === 1) {
-            $("#red").css("opacity", "1");
-            setTimeout(function () {
-                $("#red").css("opacity", ".5");
-            }, 1000);
-        } else if (simonGame[i] === 2) {
-            $("#blue").css("opacity", "1")
-            setTimeout(function () {
-                $("#blue").css("opacity", ".5");
-            }, 1000)
-        } else if (simonGame[i] === 3) {
-            $("#green").css("opacity", "1")
-            setTimeout(function () {
-                $("#green").css("opacity", ".5");
-            }, 1000)
-        } else if (simonGame[i] === 4) {
-            $("#yellow").css("opacity", "1")
-            setTimeout(function () {
-                $("#yellow").css("opacity", ".5");
-            }, 1000)
-        }
-
+              $("#red").css("opacity", "1");
+               setTimeout(function () {
+                   $("#red").css("opacity", ".5");
+               }, 1000 * (i + 1));
+           } else if (simonGame[i] === 2) {
+               $("#blue").css("opacity", "1")
+               setTimeout(function () {
+                   $("#blue").css("opacity", ".5");
+               }, 1000 * (i + 1))
+           } else if (simonGame[i] === 3) {
+               $("#green").css("opacity", "1")
+               setTimeout(function () {
+                   $("#green").css("opacity", ".5");
+               }, 1000* (i + 1))
+           } else if (simonGame[i] === 4) {
+               $("#yellow").css("opacity", "1")
+               setTimeout(function () {
+                   $("#yellow").css("opacity", ".5");
+               }, 1000 * (i + 1))
+           }
     }
     console.log(simonGame);
 }
-
- setTimeout(function(){
-        randomColor(randomNum);
-        console.log(simonGame)
-    }, 1000);
-
-    $("#red").click(function(){
+function userClick() {
+    $("#red").click(function () {
         colorClick.push(1);
         console.log(colorClick);
-        // randomNum = Math.floor(Math.random()* 4 +1);
-        setTimeout(function(){
-            if(simonGame.toString() === colorClick.toString()){
-                randomNum = Math.floor(Math.random()* 4 +1);
-                randomColor(randomNum);
-                colorClick = []
+        // setTimeout(function () {
+        //     if (simonGame.toString() === colorClick.toString()) {
+        //         colorClick = []
+        //         randomNum = Math.floor(Math.random() * 4 + 1);
+        //         randomColor(randomNum);
+        //
+        //     } else {
+        //         alert("Loser")
+        //     }
+        // }, 4000)
+        for(var j = 0; j < colorClick.length; j++) {
+            setTimeout(function () {
+                if (simonGame.toString() === colorClick.toString()) {
+                    colorClick = []
+                    randomNum = Math.floor(Math.random() * 4 + 1);
+                    randomColor(randomNum);
 
-            } else{
-                alert("Loser")
-            }
-        }, 5000)
+                } else {
+                    alert("Loser")
+                }
+            }, 4000)
+        }
 
     });
-
-    $("#blue").click(function(){
+    $("#blue").click(function () {
         colorClick.push(2);
         console.log(colorClick)
-        // randomNum = Math.floor(Math.random()* 4 +1);
-        setTimeout(function(){
-            if(simonGame.toString() === colorClick.toString()){
-                randomNum = Math.floor(Math.random()* 4 +1);
-                randomColor(randomNum);
-                colorClick = []
+        // setTimeout(function () {
+        //     if (simonGame.toString() === colorClick.toString()) {
+        //         colorClick = []
+        //         randomNum = Math.floor(Math.random() * 4 + 1);
+        //         randomColor(randomNum);
+        //
+        //     } else {
+        //         alert("Loser")
+        //     }
+        // }, 4000)
+        for(var j = 0; j < colorClick.length; j++) {
+            setTimeout(function () {
+                if (simonGame.toString() === colorClick.toString()) {
+                    colorClick = []
+                    randomNum = Math.floor(Math.random() * 4 + 1);
+                    randomColor(randomNum);
 
-            } else{
-                alert("Loser")
-            }
-        }, 6000)
+                } else {
+                    alert("Loser")
+                }
+            }, 4000)
+        }
+
     });
-
-    $("#green").click(function(){
+    $("#green").click(function () {
         colorClick.push(3)
         console.log(colorClick)
-        // randomNum = Math.floor(Math.random()* 4 +1);;
-        setTimeout(function(){
-            if(simonGame.toString() === colorClick.toString()){
-                randomNum = Math.floor(Math.random()* 4 +1);
-                randomColor(randomNum);
-                colorClick = []
+        // setTimeout(function () {
 
-            } else{
-                alert("Loser")
-            }
-        }, 6000)
+        //     if (simonGame.toString() === colorClick.toString()) {
+        //         colorClick = [];
+        //
+        //         randomNum = Math.floor(Math.random() * 4 + 1);
+        //         randomColor(randomNum);
+        //
+        //     } else {
+        //         alert("Loser")
+        //     }
+        // }, 4000)
+        for(var j = 0; j < colorClick.length; j++) {
+            setTimeout(function () {
+                if (simonGame.toString() === colorClick.toString()) {
+                    colorClick = []
+                    randomNum = Math.floor(Math.random() * 4 + 1);
+                    randomColor(randomNum);
+
+                } else {
+                    alert("Loser")
+                }
+            }, 4000)
+        }
+
     });
-
-    $("#yellow").click(function(){
+    $("#yellow").click(function () {
         colorClick.push(4);
         console.log(colorClick)
-        // randomNum = Math.floor(Math.random()* 4 +1);
-        setTimeout(function(){
-            if(simonGame.toString() === colorClick.toString()){
-                randomNum = Math.floor(Math.random()* 4 +1);
-                randomColor(randomNum);
-                colorClick = []
+        // setTimeout(function () {
+        //     if (simonGame.toString() === colorClick.toString()) {
+        //         colorClick = []
+        //         randomNum = Math.floor(Math.random() * 4 + 1);
+        //         randomColor(randomNum);
+        //
+        //     } else {
+        //         alert("Loser")
+        //     }
+        // }, 4000)
+        for(var j = 0; j < colorClick.length; j++) {
+            setTimeout(function () {
+                if (simonGame.toString() === colorClick.toString()) {
+                    colorClick = []
+                    randomNum = Math.floor(Math.random() * 4 + 1);
+                    randomColor(randomNum);
 
-            } else{
-                alert("Loser")
-            }
-        }, 6000)
+                } else {
+                    alert("Loser")
+                }
+            }, 4000)
+        }
 
     });
+    for(var j = 0; j < colorClick.length; j++) {
+        setTimeout(function () {
+            if (simonGame.toString() === colorClick.toString()) {
+                colorClick = []
+                randomNum = Math.floor(Math.random() * 4 + 1);
+                randomColor(randomNum);
+                userClick();
 
-    setTimeout(function(){
-        if(simonGame.toString() === colorClick.toString()){
-            randomColor(randomNum);
-            colorClick = []
-        } else{
-            alert("Loser")
-        }
-    }, 6000)
-
-    console.log(colorClick);
-
-
-
+            } else {
+                alert("Loser")
+            }
+        }, 4000)
+    }
+}
 })
